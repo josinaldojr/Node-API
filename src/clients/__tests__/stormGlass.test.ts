97dd7505f75a7b456b996c53153269ce75dd8d8d
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2020-08-26 14:28:55 -0300

    feat: creating client API to consume the external API

---
+++ src/clients/__tests__/stormGlass.test.ts
@@ -0,0 +1,19 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixtures from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixtures from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('shold return the normalized forecast from the StromGlass service', async () => {
+    const lat = -33.7927726;
+    const lng = 151.289824;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixtures);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalized3HoursFixtures);
+  });
+})
